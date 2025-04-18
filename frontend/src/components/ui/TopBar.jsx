f33edafec608506754f840280227d4ad2e955e37
Danco <dancornioo@gmail.com>
2025-04-18 14:24:02 -0300

    motions in top bar

--- frontend/src/components/ui/TopBar.jsx
+++ frontend/src/components/ui/TopBar.jsx
@@ -3,18 +3,29 @@ import MidAccountAlert from "../svgs/MidAccountAlert";
 import Shopping from "../svgs/Shopping";
 import SearchIcon from "../svgs/SearchIcon";
 import HeartIcon from "../svgs/HeartIcon";
+import { Prompt } from "next/font/google";
 
 const roboto = Roboto({
   subsets: ['latin'],
   weight: '400'
 });
 
+const prompt = Prompt({
+  subsets: ['latin'],
+  weight: '400' 
+})
+
 export default function TopBar() {
   return (
     <nav className="relative w-full h-[5rem]">
-        <div className="fixed w-full h-[5rem] top-0 left-0 bg-[#f5e2a5] z-10">
+        <div className="fixed w-full h-[5rem] top-0 left-0 bg-[#FBEBB5] z-10">
 
-          <ul className={`${roboto.className} absolute w-[431px] h-6 top-[38px] left-1/2 transform -translate-x-1/2 flex justify-between`} >
+          <div className="w-36 h-9 absolute left-1/16 top-1/2 transform  -translate-y-1/2">
+            <div className="w-[98px] h-0 left-[2px] top-[32px] absolute  outline-1 outline-offset-[-0.50px] outline-black"></div>
+            <a className= {`${prompt.className} left-0 top-0 absolute justify-start text-black text-2xl font-normal  motion-preset-bounce `}>CHIC BRASIL</a>
+          </div>
+
+          <ul className={`${roboto.className} absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center w-[431px] h-6`} >
             <li className="font-poppins font-semibold text-black text-base tracking-normal leading-normal px-4 ">
               <a href="#Home">Home</a>
             </li>
@@ -29,15 +40,14 @@ export default function TopBar() {
             </li>
           </ul>
             
-          <div className="absolute w-[247px] h-7 top-9 left-4/5">
-            <ul className="absolute w-7 h-7 top-0  transform -translate-x-1.5 flex justify-between gap-[2.813rem]" >
-              <li className="tracking-normal leading-normal motion-safe:hover:animate-bounce"><a href="#Conta"><MidAccountAlert/></a></li>
-              <li className="tracking-normal leading-normal"><a href="#Shopping"><Shopping/></a></li>
-              <li className="tracking-normal leading-normal"><a href="#busca"><SearchIcon/></a></li>
-              <li className="tracking-normal leading-normal"><a href="#Lista"><HeartIcon/></a></li>
+          <div className="absolute w-[11.125rem] h-7 top-1/2 left-4/5 transform -translate-y-1/2">
+            <ul className="flex justify-between gap-[1.375rem] items-center w-full h-full">
+              <li className="tracking-normal leading-normal motion-safe:hover:motion-preset-seesaw"><a href="#Conta"><MidAccountAlert/></a></li>
+              <li className="tracking-normal leading-normal motion-safe:hover:motion-preset-seesaw"><a href="#Shopping"><Shopping/></a></li>
+              <li className="tracking-normal leading-normal motion-safe:hover:motion-preset-seesaw"><a href="#busca"><SearchIcon/></a></li>
+              <li className="tracking-normal leading-normal motion-safe:hover:motion-preset-seesaw"><a href="#Lista"><HeartIcon/></a></li>
             </ul>
           </div>
-        
         </div>
       </nav>
   );
